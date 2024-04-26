fae60ba7073dd1e45204b919a72e9477b17df957
Adesoji Daniel <sojidaniels@gmail.com>
2024-04-26 13:27:52 +0100

    initial commit

--- start/env.ts
+++ start/env.ts
@@ -16,12 +16,4 @@ export default await Env.create(new URL('../', import.meta.url), {
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string(),
-
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring session package
-  |----------------------------------------------------------
-  */
-  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
 })
